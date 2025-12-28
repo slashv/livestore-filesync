8ea4e9b2906d22d4f2f9a02d043d7011dc4d17cd
Victor Nyberg <contact@victornyberg.com>
2025-12-28 10:29:32 +0100

    firefox e2e proj

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -19,6 +19,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 
   webServer: {
